e1acc0ff711dcd676a407f2c7d3ee00bfc78778f
Niko <rappe.niko@gmail.com>
2023-05-03 14:06:46 +0300

    All things done

--- index.js
+++ index.js
@@ -48,28 +48,31 @@ app.get('/api/persons', (req, res) => {
     })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    let id = Number(req.params.id)
-    let person = persons.find(p => p.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+app.get('/api/persons/:id', (req, res, next) => {
+    Person.findById(req.params.id)
+      .then(person => {
+        if (person) {
+          res.json(person)
+        } else {
+          res.status(404).end()
+        }
+      })
+      .catch(error => next(error))
 })
 
 app.get('/info', (req, res) => {
-    let count = persons.length
     let now = new Date()
-    res.send(`
-        <p>Phonebook has info for ${count} people.</p>
+    Person.find({}).then(persons => {
+      res.send(`
+        <p>Phonebook has info for ${persons.length} people</p>
         <p>${now.toString()}</p>`)
+    })
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     let body = req.body
 
-    if(body.name === undefined || !body.number === undefined) {
+    if(!body.name || !body.number) {
         return res.status(400).json({
             error: 'Name or number missing!'
         })
@@ -83,14 +86,43 @@ app.post('/api/persons', (req, res) => {
     person.save().then(savedPerson => {
       res.json(savedPerson)
     })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (req, res) => {
-    let id = Number(req.params.id)
-    persons = persons.filter(p => p.id !== id)
-
-    res.status(204).end()
+app.put('/api/persons/:id', (req, res, next) => {
+    let body = req.body
+    let person = {
+      name: body.name,
+      number: body.number,
+    }
+    Person.findByIdAndUpdate(req.params.id, person, {new: true})
+      .then(updatedPerson => {
+        res.json(updatedPerson)
+      })
+      .catch(error => next(error))
 })
+
+app.delete('/api/persons/:id', (req, res, next) => {
+    Person.findByIdAndRemove(req.params.id)
+      .then(result => {
+        res.status(204).end()
+      })
+      .catch(error => next(error))
+})
+
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  } else if(error.name === 'ValidationError') {
+    return res.status(400).json({error: error.message})
+  }
+  next(error)
+}
+  
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
